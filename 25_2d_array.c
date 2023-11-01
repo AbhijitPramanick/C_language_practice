4ad5de75895d2c6a0406897cd86a72bf2bb37991
AbhijitPramanick <amanik1995@gmail.com>
2023-11-01 15:45:41 +0530

    Understand 2 D array in C

---
+++ 25_2d_array.c
@@ -0,0 +1,26 @@
+// WAP to demonstrate 2d array
+
+#include <stdio.h>
+
+int main()
+{
+    int a[3][3];
+    printf("Enter the elements of the array : ");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("The contents of the array :\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
